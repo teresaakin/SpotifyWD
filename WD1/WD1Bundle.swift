997163eddb4e7626ee8294ca7fe6ff969af4e369
Teresa <akinbodt@mcmaster.ca>
2024-09-21 12:02:15 -0400

    placeholders for the widget have been included

---
+++ WD1/WD1Bundle.swift
@@ -0,0 +1,16 @@
+//
+//  WD1Bundle.swift
+//  WD1
+//
+//  Created by Teresa Akinbodun on 2024-09-19.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WD1Bundle: WidgetBundle {
+    var body: some Widget {
+        WD1()
+    }
+}
